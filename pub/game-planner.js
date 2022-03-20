aea06db7550a67abc2d50345b80acaa93803298a
Yiyun Ding <yiyun.ding@mail.utoronto.ca>
2022-03-20 13:08:08 -0400

    basic functionality

--- pub/game-planner.js
+++ pub/game-planner.js
@@ -23,7 +23,7 @@ class Event {
 
 function GamePlanner() {
 	this.events = [];
-    this.filterVariables = ['NA', 'player', 'time'];
+    this.filterVariables = ['NA', 'player'];
     this.numberOfEvents = 0;
     this.tableName = null;
 }
@@ -32,6 +32,7 @@ function GameRenderer(gamePlanner, container) {
 	if (!(gamePlanner instanceof GamePlanner)) {
 		throw new Error('Initialize renderer using a GamePlanner');
 	}
+    this.filteredEventList = gamePlanner.events;
     this.container = container;
 	this.gamePlanner = gamePlanner;
     this.showByWeeks = true;
@@ -82,7 +83,10 @@ GameRenderer.prototype = {
         this.monthsButton.innerHTML = 'Months';
         this.filterButton = document.createElement('button');
         this.filterButton.innerHTML = 'filter';
-        this.filterButton.id = 'filter';
+        this.filterButton.className = 'filter';
+        this.addFilterButton = document.createElement('button');
+        this.addFilterButton.innerHTML = 'add filter';
+        this.addFilterButton.id = 'add-filter';
         this.lastButton = document.createElement('button');
         this.lastButton.innerHTML = 'last';
         this.nextButton = document.createElement('button');
@@ -99,39 +103,215 @@ GameRenderer.prototype = {
             this.showByWeeks = false;
             this.display();
         });
-        this.filterButton.addEventListener('click', this.filterOnClick);
+        this.filterButton.addEventListener('click', () => {
+            this.filteredEventList = this.gamePlanner.events;
+            let filterDiv = document.getElementById('filterDiv');
+            let li;
+            for (li of filterDiv.children[2].children){
+                let operationSelect = li.getElementsByClassName('filter-operation')[0];
+                let variableSelect = li.getElementsByClassName('filter-variable')[0];
+                let operation = operationSelect.options[operationSelect.selectedIndex].text;
+                let variable = variableSelect.options[variableSelect.selectedIndex].text;
+                let value = li.getElementsByClassName('filter-input')[0].value;
+                if (operation != 'NA' && variable != 'NA') {
+                    this._filterEvents(variable, value, operation);
+                }
+            }
+            this.display();
+            
+        });
+        this.addFilterButton.addEventListener('click', () => {
+            let filterUl = document.getElementById('filterDiv').children[2];
+            filterUl.appendChild(this._createFilterLi());
+        });
+        
         this.lastButton.addEventListener('click', () => {
             if (this.showByWeeks == true) {
-                console.log(this.startDate);
                 this.startDate = new Date(this.startDate.setDate(this.startDate.getDate() - 7));
-                console.log(this.startDate);
             } 
             // TODO for beta phase.
             // else {
             // }
             this.display();
-        })
+        });
         this.nextButton.addEventListener('click', () => {
             if (this.showByWeeks == true) {
-                console.log(this.startDate);
                 this.startDate = new Date(this.startDate.setDate(this.startDate.getDate() + 7));
-                console.log(this.startDate);
             } 
             // TODO for beta phase.
             // else {
             // }
             this.display();
-        })
+        });
 
     },
 
-    _filterOnClick: function() {
-        let w = window.open("");
-        w.document.writeln("popup-window");
+    _eventExists: function(variable, value){
+        let result_list = [];
+        for (event of this.filteredEventList){
+            if (variable == 'player') {
+                if (event.player1 != null && event.player2 != null){
+                    result_list.push(event);
+                }
+            }
+            else if (variable == 'duration') {
+                if (event.duration != null){
+                    result_list.push(event);
+                }
+            }
+            else if (variable == 'type') {
+                if (event.type != null){
+                    result_list.push(event);
+                }
+            }
+            else if (variable == 'location') {
+                if (event.location != null){
+                    result_list.push(event);
+                }
+            }
+            else if (variable == 'result') {
+                if (event.result != null){
+                    result_list.push(event);
+                }
+            }
+            else if (variable == 'gamename') {
+                if (event.gamename != null){
+                    result_list.push(event);
+                }
+            }
+            //TODO: add more
+        }
+        this.filteredEventList = result_list;
     },
+
+    _eventIs: function(variable, value){
+        let result_list = [];
+        for (event of this.filteredEventList){
+            if (variable == 'player') {
+                if (event.player1 == value || event.player2 == value){
+                    result_list.push(event);
+                }
+            }
+            else if (variable == 'duration') {
+                if (event.duration == value){
+                    result_list.push(event);
+                }
+            }
+            else if (variable == 'type') {
+                if (event.type == value){
+                    result_list.push(event);
+                }
+            }
+            else if (variable == 'location') {
+                if (event.location == value){
+                    result_list.push(event);
+                }
+            }
+            else if (variable == 'result') {
+                if (event.result == value){
+                    result_list.push(event);
+                }
+            }
+            else if (variable == 'gamename') {
+                if (event.gamename == value){
+                    result_list.push(event);
+                }
+            }
+            //TODO: add more
+        }
+        this.filteredEventList = result_list;
+    },
+
+    _eventIsNot: function(variable, value){
+        let result_list = [];
+        for (event of this.filteredEventList){
+            if (variable == 'player') {
+                if (event.player1 != value && event.player2 != value){
+                    result_list.push(event);
+                }
+            }
+            else if (variable == 'duration') {
+                if (event.duration != value){
+                    result_list.push(event);
+                }
+            }
+            else if (variable == 'type') {
+                if (event.type != value){
+                    result_list.push(event);
+                }
+            }
+            else if (variable != 'location') {
+                if (event.location == value){
+                    result_list.push(event);
+                }
+            }
+            else if (variable != 'result') {
+                if (event.result == value){
+                    result_list.push(event);
+                }
+            }
+            else if (variable != 'gamename') {
+                if (event.gamename == value){
+                    result_list.push(event);
+                }
+            }
+            //TODO: add more
+        }
+        this.filteredEventList = result_list;
+    },
+
+    _filterEvents: function(variable, value, operation){
+        let result_list = [];
+        let event;
+        for (event of this.filteredEventList){
+            console.log(this.filteredEventList);
+            if (operation == 'exists'){
+                this._eventExists(variable, value);
+            }
+            else if (operation == 'is'){
+                this._eventIs(variable, value);
+            }
+            else if (operation == 'is not'){
+                this._eventIsNot(variable, value);
+            }
+        }
+    },
+
 
     setTableType: function(showByWeeks) {
         this.showByWeeks = showByWeeks;
+    },
+
+    _createFilterLi: function() {
+        let filterLi = document.createElement('li');
+        let filterDescription = document.createElement('span');
+        filterDescription.innerHTML = 'filter your events here';
+        let variableSelect = document.createElement('select');
+        variableSelect.className = 'filter-variable';
+        let filterVariable;
+        for (filterVariable of gamePlanner.filterVariables){
+            let filterOption = document.createElement('option');
+            filterOption.innerHTML = filterVariable;
+            variableSelect.appendChild(filterOption);
+        }
+        filterLi.appendChild(variableSelect);
+
+        operationSelect = document.createElement('select');
+        operationSelect.className = 'filter-operation';
+        let operation;
+        for (operation of ['NA', 'exists', 'is', 'is not']){
+            let operationOption = document.createElement('option');
+            operationOption.innerHTML = operation;
+            operationSelect.appendChild(operationOption);
+        }
+        filterLi.appendChild(operationSelect);
+
+        let input = document.createElement('input');
+        input.type = 'text';
+        input.className = 'filter-input';
+        filterLi.appendChild(input);
+        return filterLi;
+
     },
 
     display: function() {
@@ -173,36 +353,13 @@ GameRenderer.prototype = {
 
         // filter bar
         filterDiv = document.createElement('div');
-        filterDiv.id = 'filter';
-        
-        filterDescription = document.createElement('span');
-        filterDescription.innerHTML = 'filter your events here';
-        variableSelect = document.createElement('select');
-        variableSelect.id = 'filter-variable';
-        let filterVariable;
-        for (filterVariable of gamePlanner.filterVariables){
-            let filterOption = document.createElement('option');
-            filterOption.innerHTML = filterVariable;
-            variableSelect.appendChild(filterOption);
-        }
-        filterDiv.appendChild(variableSelect);
-
-        operationSelect = document.createElement('select');
-        operationSelect.id = 'filter-operation';
-        let operation;
-        for (operation of ['NA', 'exists', 'is', 'is not']){
-            let operationOption = document.createElement('option');
-            operationOption.innerHTML = operation;
-            operationSelect.appendChild(operationOption);
-        }
-        filterDiv.appendChild(operationSelect);
-
-        let input = document.createElement('input');
-        input.type = 'text';
-        input.id = 'filter-input';
-        filterDiv.appendChild(input);
-
+        filterDiv.id = 'filterDiv';
         filterDiv.appendChild(this.filterButton);
+        filterDiv.appendChild(this.addFilterButton);
+        filterUl = document.createElement('ul');
+        filterUl.appendChild(this._createFilterLi())
+        
+        filterDiv.appendChild(filterUl);
         gamePlannerDiv.appendChild(filterDiv);
 
         // main table
@@ -214,14 +371,12 @@ GameRenderer.prototype = {
         }
         this.container.innerHTML = ''; //clear everything
         this.container.appendChild(gamePlannerDiv);
-        console.log(gamePlannerDiv);
     },
 
     _filterEventsWithDate: function(date) {
-        let eventList = this.gamePlanner.events;
         let event;
         let result = [];
-        for (event of eventList) {
+        for (event of this.filteredEventList) {
             if ((event.startTime).getDate() == date.getDate() && (event.startTime).getMonth() == date.getMonth() && (event.startTime).getYear() == date.getYear()){
                 result.push(event);
             }
@@ -265,7 +420,6 @@ GameRenderer.prototype = {
             event_td.className = 'events';
             let date = new Date(this.startDate);
             date = new Date(date.setDate(date.getDate() + i));
-            console.log(date);
             let event;
             for (event of this._filterEventsWithDate(date)){
                 eventDiv = document.createElement('div');
